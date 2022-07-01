d49f595a62efef82d1d0361a2333ff5eabe595c2
HBilel101 <hbilel992@gmail.com>
2022-07-01 19:34:29 +0100

    make it better

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.put('/image',(req,res)=>{image.handleImage(req,res,postgres)
 app.post('/imageUrl',(req,res)=>{image.handleImageApi(req,res,postgres)
 })
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log(`app is running on port ${process.env.PORT}`);
 })
