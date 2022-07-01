34a1cb6205ace7fe27d99d2b3a77607e48d69cf5
HBilel101 <hbilel992@gmail.com>
2022-07-01 20:13:36 +0100

    removing nodemon from start script

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.get('/',(req,res)=>{
-    res.json('it works')
+    res.json('it works');
 })
 app.post('/signin',(req,res)=>{
 signin.handleSignin(req,res,postgres,bcrypt);
