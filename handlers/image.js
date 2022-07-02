89d419c5a72b7ed7d9c0bc23128c91395e9654ac
HBilel101 <hbilel992@gmail.com>
2022-07-02 12:01:37 +0100

    fixed error res.status.json() is not a function

--- handlers/image.js
+++ handlers/image.js
@@ -22,7 +22,7 @@ postgres('users').where('id','=',id)
 })
 .returning('entries')
 .then(entries=>res.json(entries[0].entries))
-.catch(err=>res.status.json('unable to get entries'))}
+.catch(err=>res.status(400).json('unable to get entries'))}
 
 
 
